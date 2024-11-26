785f869e97b5f170242265a8a7dbbf5c3bb366f1
Jim Burgess <jim@tempomobile.co.uk>
2024-11-26 10:50:36 +0000

    wip

--- src/navigation.ts
+++ src/navigation.ts
@@ -8,14 +8,14 @@ export const headerData = {
     },
     {
       text: 'Work With Us',
-      href: getWorkPermalink(), // Use the getWorkPermalink function
+      href: getWorkPermalink(), 
     },
     {
       text: 'Blog',
       href: getBlogPermalink(),
     },
   ],
-  actions: [{ text: 'Book Now', href: '#', target: '_blank' }],
+  actions: [{ text: 'Book Now', href: './contact', target: 'primary' }],
 };
 
 export const footerData = {
@@ -30,6 +30,7 @@ export const footerData = {
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-white underline dark:text-muted" href="#ß"> James</a> · All rights reserved.
+    Made by <a class="text-white underline dark:text-muted" href="#ß
+    "></a> · All rights reserved.
   `,
 };
