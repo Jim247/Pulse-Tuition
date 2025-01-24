a99285410d537056bac76501494abc352a19d5c8
Jim Burgess <jim@tempomobile.co.uk>
2025-01-24 15:43:21 +0000

    footer tidy

--- src/navigation.ts
+++ src/navigation.ts
@@ -42,12 +42,11 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/pulse.tuition', class: 'w-8 h-8' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/pulse.tuition.uk', class: 'w-8 h-8' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-white underline dark:text-muted" href="#ß
+    Pulse Tuition <a class="text-white underline dark:text-muted" href="#ß
     "></a> · All rights reserved.
   `,
 };
