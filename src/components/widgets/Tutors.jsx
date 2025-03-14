0e2d40f681af6bc58b6331e982d94c217a931344
Jim Burgess <jim@tempomobile.co.uk>
2025-03-14 15:29:30 +0000

    obj-top tutor photos

--- src/components/widgets/Tutors.jsx
+++ src/components/widgets/Tutors.jsx
@@ -112,7 +112,13 @@ function Tutors() {
             className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow flex flex-col"
           >
             {/* Tutor Photo */}
-            <img src={tutor.photo} alt={`${tutor.name} photo`} className="w-full h-64 object-cover object-center" />
+            <div className="w-full h-64 overflow-hidden">
+              <img
+                src={tutor.photo}
+                alt={tutor.name}
+                className="w-full h-full object-cover object-top"
+              />
+            </div>
 
             {/* Tutor Details */}
             <div className="p-4 flex flex-col flex-1">
